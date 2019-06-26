35f4d02d36dd584d74b0f4ccc3be17b1fec3a139
Guillaume Benard <guillaume.benard@epitech.eu>
2019-06-26 15:17:37 +0200

    [DB] Create basic User table

---
+++ src/models/entities/User.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity("users")
+class User {
+    @PrimaryGeneratedColumn()
+    public id: number;
+
+    @Column()
+    @Index()
+    public email: string;
+
+    @Column()
+    public name: string;
+
+    @Column({ name: "ciphered_password" })
+    public cipheredPassword: string;
+
+    constructor(data?: UserData) {
+        this.id = 0;
+        this.email = data && data.email || "";
+        this.name = data && data.name || "";
+        this.cipheredPassword = data && data.cipheredPassword || "";
+    }
+}
+
+type UserData = {
+    email: string,
+    name: string,
+    cipheredPassword: string
+};
+
+
+export default User;
---
+++ src/models/migrations/1561554862458-CreateUserTable.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUserTable1561554862458 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "name" character varying NOT NULL, "ciphered_password" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE INDEX "IDX_97672ac88f789774dd47f7c8be" ON "users" ("email") `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`DROP INDEX "IDX_97672ac88f789774dd47f7c8be"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
