e27d1be8c0f64355fef6db61612a24eb33486165
Guillaume Benard <guillaume.benard@epitech.eu>
2019-06-27 17:49:21 +0200

    Add JwtService

---
+++ src/services/JwtService.ts
@@ -0,0 +1,23 @@
+import Jwt from "jsonwebtoken";
+
+
+class JwtService {
+
+    private readonly privateKey: string;
+
+    constructor(privateKey: string) {
+        this.privateKey = privateKey;
+    }
+
+    public readonly tokenize = (data: any): string => {
+        return Jwt.sign({ data: data }, this.privateKey);
+    };
+
+    public readonly decode = (token: string): any => {
+        const { data: data } = <any>Jwt.verify(token, this.privateKey);
+        return data;
+    };
+}
+
+
+export default JwtService;
