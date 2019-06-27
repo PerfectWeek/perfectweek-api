2640fc1ab197372733d245a78dfa7f0e205bd9cd
Guillaume Benard <guillaume.benard@epitech.eu>
2019-06-27 17:49:05 +0200

    Add PasswordService
    - Password ciphering
    - Password auhorization

---
+++ src/services/PasswordService.ts
@@ -0,0 +1,18 @@
+import Bcrypt from "bcrypt";
+
+
+class PasswordService {
+
+    private static readonly NB_ROUNDS: number = 10;
+
+    public readonly cipherPassword = async (password: string): Promise<string> => {
+        return Bcrypt.hash(password, PasswordService.NB_ROUNDS);
+    };
+
+    public readonly validatePassword = async (cipheredPassword: string, password: string): Promise<boolean> => {
+        return Bcrypt.compare(password, cipheredPassword);
+    };
+}
+
+
+export default PasswordService;
