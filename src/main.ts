8fae12d575747e01dd9ede038f8d4f3adaf270a3
Guillaume Benard <guillaume.benard@epitech.eu>
2019-06-27 17:55:34 +0200

    Edit setup in router
    - Create services
    - Create repositories
    - Create controllers

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,19 @@ import * as Router from "./router";
 import Server from "./Server";
 
 import ApiEndpointController from "./controllers/ApiEndpointController";
+import AuthLocalController from "./controllers/AuthLocalController";
+import UserController from "./controllers/UserController";
+
+import JwtService from "./services/JwtService";
+import PasswordService from "./services/PasswordService";
+
+import UserRepository from "./models/UserRepository";
+
+import EmailValidator from "./validators/EmailValidator";
+import NameValidator from "./validators/NameValidator";
+import PasswordValidator from "./validators/PasswordValidator";
+
+import * as AuthenticatedOnly from "./middlewares/authenticatedOnly";
 
 
 const API_PORT: number = 3000;
@@ -14,24 +27,58 @@ const API_PORT: number = 3000;
 function main(): void {
     const dbConfig = DbConfig.load("../ormconfig.js");
 
+    const jwtSecretKey = process.env.JWT_SECRET_KEY;
+    if (!jwtSecretKey) {
+        throw new Error('Missing environment variable "JWT_SECRET_KEY"');
+    }
+
     createConnection(dbConfig)
         .then((conn: Connection) => {
             console.info("Connected to database");
 
-            const server = createServer(conn);
+            const server = createServer(conn, jwtSecretKey);
 
             server.start(API_PORT);
         });
 }
 
-function createServer(_conn: Connection): Server {
+function createServer(conn: Connection, jwtSecretKey: string): Server {
+    // Create Repositories
+    const userRepository = new UserRepository(conn);
 
-    // Controllers
+    // Create Validators
+    const emailValidator = new EmailValidator();
+    const nameValidator = new NameValidator();
+    const passwordValidator = new PasswordValidator();
+
+    // Create Services
+    const jwtService = new JwtService(jwtSecretKey);
+    const passwordService = new PasswordService();
+
+    // Create Controllers
     const apiEndpointController = new ApiEndpointController();
+    const authLocalController = new AuthLocalController(
+        userRepository,
+        jwtService,
+        passwordService,
+        emailValidator,
+        nameValidator,
+        passwordValidator
+    );
+    const userController = new UserController();
+
+    // Create middlewares
+    const authenticatedOnlyMiddleware = AuthenticatedOnly.generateMiddleware(userRepository, jwtService);
 
     // Create Router
     const router = Router.createRouter(
-        apiEndpointController
+        // Controller
+        apiEndpointController,
+        authLocalController,
+        userController,
+
+        // Middleware
+        authenticatedOnlyMiddleware
     );
 
     return new Server(router);
--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,30 @@
 import { Router } from "express";
 
 import ApiEndpointController from "./controllers/ApiEndpointController";
+import AuthLocalController from "./controllers/AuthLocalController";
+import UserController from "./controllers/UserController";
+
+import { AuthenticatedOnlyMiddleware } from "./middlewares/authenticatedOnly";
+import asyncHandler from "./middlewares/utils/asyncHandler";
 
 
 export function createRouter(
     // Controllers
-    apiEndpointController: ApiEndpointController
+    apiEndpointController: ApiEndpointController,
+    authLocalController: AuthLocalController,
+    userController: UserController,
+
+    // Middleware
+    authenticatedOnly: AuthenticatedOnlyMiddleware
 ): Router {
     const router = Router();
 
     router.get("/", apiEndpointController.endpoint);
+
+    router.post("/auth/local/register", asyncHandler(authLocalController.registerUser));
+    router.post("/auth/local/login", asyncHandler(authLocalController.loginUser));
+
+    router.get("/users/me", asyncHandler(authenticatedOnly), userController.getMyInfo);
 
     return router;
 }
