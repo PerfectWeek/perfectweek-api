bcf89fe23ba281eed5728474db77c073acb6d4dc
Guillaume Benard <guillaume.benard@epitech.eu>
2019-07-06 16:05:51 +0200

    [DB] Edit ormconfig
    Handle either DATABASE_URL
    or DB separated info fields (host, port, ...)

--- ormconfig.js
+++ ormconfig.js
@@ -1,17 +1,48 @@
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+//
+// Configure connection from environment
+//
+const connectionOptions = {};
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (DATABASE_URL) {
+    connectionOptions.url = DATABASE_URL;
+} else {
+    const DB_HOST = process.env.DB_HOST;
+    const DB_PORT = process.env.DB_PORT;
+    const DB_NAME = process.env.DB_NAME;
+    const DB_USER = process.env.DB_USER;
+    const DB_PASSWORD = process.env.DB_PASSWORD;
+
+    if (DB_HOST && DB_PORT && DB_NAME && DB_USER && DB_PASSWORD) {
+        connectionOptions.host = DB_HOST;
+        connectionOptions.port = DB_PORT;
+        connectionOptions.database = DB_NAME;
+        connectionOptions.username = DB_USER;
+        connectionOptions.password = DB_PASSWORD;
+    } else {
+        throw new Error(
+            "Missing database configuration, the following environment variable must be provided:\n" +
+            "    - Either:\n" +
+            "        DATABASE_URL\n" +
+            "    - Or:\n" +
+            "        DB_HOST\n" +
+            "        DB_PORT\n" +
+            "        DB_NAME\n" +
+            "        DB_USER\n" +
+            "        DB_PASSWORD\n"
+        );
+    }
+}
 
 
+//
+// Export DB connection options
+//
 module.exports = {
     type: "postgres",
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME,
-    username: DB_USER,
-    password: DB_PASSWORD,
+
+    ...connectionOptions,
 
     synchronize: false,
     logging: false,
