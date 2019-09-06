0beb47433502ecef01312b2b4b0eac7beb9f7595
Guillaume Benard <guillaume@benard.io>
2019-09-06 16:36:34 +0200

    Add favicon

--- sources/controllers/ApiEndpointController.ts
+++ sources/controllers/ApiEndpointController.ts
@@ -2,9 +2,22 @@ import { Request, Response } from "express";
 
 export class ApiEndpointController {
 
+    private readonly faviconImage: string;
+
+    constructor(
+        // Images
+        faviconImage: string,
+    ) {
+        this.faviconImage = faviconImage;
+    }
+
     public readonly endpoint = (_req: Request, res: Response) => {
         res.status(200).json({
             message: "PerfectWeek API du swag !",
         });
+    }
+
+    public readonly favicon = (_req: Request, res: Response) => {
+        res.status(200).sendFile(this.faviconImage);
     }
 }
--- sources/router.ts
+++ sources/router.ts
@@ -38,6 +38,10 @@ export function createRouter(
         "/",
         apiEndpointController.endpoint,
     );
+    router.get(
+        "/favicon.ico",
+        apiEndpointController.favicon,
+    );
 
     //
     // Auth (local)
