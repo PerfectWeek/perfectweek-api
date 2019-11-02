118c194ba24da18d1c1bf23e2226ecb56a4542b0
Guillaume Benard <guillaume@benard.io>
2019-11-02 16:38:35 +0100

    Add mailing and Mailgun to config

--- sources/config.ts
+++ sources/config.ts
@@ -1,4 +1,5 @@
 import { envOrThrow } from "./utils/envOrThrow";
+import { parseBool } from "./utils/parseBool";
 
 // tslint:disable-next-line: interface-name
 export interface Config {
@@ -7,12 +8,42 @@ export interface Config {
     readonly JWT_SECRET_KEY: string;
 
     readonly ASSETS_ROOT_DIR: string;
+
+    readonly EMAIL_ENABLED: boolean;
+    readonly EMAIL_FROM: string;
+    readonly MAILGUN_API_KEY?: string;
+    readonly MAILGUN_DOMAIN?: string;
+
+    readonly FRONT_END_HOST: string;
 }
 
 export function loadConfig(): Config {
+    // Mailer
+    const EMAIL_ENABLED = parseBool(envOrThrow("EMAIL_ENABLED", "false"));
+    if (EMAIL_ENABLED === undefined) {
+        throw new Error("Invalid value for env variable EMAIL_ENABLED");
+    }
+    let EMAIL_FROM = "";
+    let MAILGUN_API_KEY: string | undefined;
+    let MAILGUN_DOMAIN: string | undefined;
+    if (EMAIL_ENABLED) {
+        EMAIL_FROM = envOrThrow("EMAIL_FROM");
+        MAILGUN_API_KEY = envOrThrow("MAILGUN_API_KEY");
+        MAILGUN_DOMAIN = envOrThrow("MAILGUN_DOMAIN");
+    }
+
     return {
         API_PORT: Number.parseInt(envOrThrow("PORT", "3000"), 10),
+
         JWT_SECRET_KEY: envOrThrow("JWT_SECRET_KEY"),
+
         ASSETS_ROOT_DIR: envOrThrow("ASSETS_ROOT_DIR"),
+
+        EMAIL_ENABLED: EMAIL_ENABLED,
+        EMAIL_FROM: EMAIL_FROM,
+        MAILGUN_API_KEY: MAILGUN_API_KEY,
+        MAILGUN_DOMAIN: MAILGUN_DOMAIN,
+
+        FRONT_END_HOST: envOrThrow("FRONT_END_HOST"),
     };
 }
