407164ea8248f5444b04352ebc699dd17b9b8b91
Guillaume Benard <guillaume@benard.io>
2019-11-02 16:40:11 +0100

    Send confirmation mail on account creation
    Closes #37

--- sources/services/MailService.ts
+++ sources/services/MailService.ts
@@ -3,20 +3,24 @@ import Mailgun from "mailgun-js";
 export class MailService {
 
     private readonly mailgun: Mailgun.Mailgun;
+    private readonly fromEmail: string;
 
-    constructor(mailgun: Mailgun.Mailgun) {
+    constructor(mailgun: Mailgun.Mailgun, fromEmail: string) {
         this.mailgun = mailgun;
+        this.fromEmail = fromEmail;
     }
 
     public readonly sendEmail = async (
         emailData: EmailData,
     ): Promise<Mailgun.messages.SendResponse> => {
-        return this.mailgun.messages().send(emailData);
+        return this.mailgun.messages().send({
+            ...emailData,
+            from: this.fromEmail,
+        });
     }
 }
 
 export type EmailData = {
-    from: string;
     to: string;
     subject: string;
     text: string;
@@ -25,7 +29,8 @@ export type EmailData = {
 export function createMailService(
     mailgunApiKey: string,
     mailgunDomain: string,
+    fromEmail: string,
 ): MailService {
     const mailgun = Mailgun({ apiKey: mailgunApiKey, domain: mailgunDomain });
-    return new MailService(mailgun);
+    return new MailService(mailgun, fromEmail);
 }
